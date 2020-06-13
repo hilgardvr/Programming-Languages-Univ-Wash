f7f38b1cb266f7208d6ab289cd96b913cdb64511
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-06-13 17:21:05 +0200

    wip 2

---
+++ part_c/week1/hw6assignment.rb
@@ -0,0 +1,37 @@
+# University of Washington, Programming Languages, Homework 6, hw6runner.rb
+
+# This is the only file you turn in, so do not modify the other files as
+# part of your solution.
+
+class MyPiece < Piece
+  # The constant All_My_Pieces should be declared here
+
+  # your enhancements here
+  All_Pieces = [[0, 0], [1, 0], [0, 1], [1, 1]]  # square (only needs one)
+#  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+#               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+#               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+#               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+#               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+#               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+#               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+#               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+
+end
+
+class MyBoard < Board
+  # your enhancements here
+
+end
+
+class MyTetris < Tetris
+  # your enhancements here
+  def key_bindings
+    super()
+    @root.bind('u', proc {@board.rotate_counter_clockwise})
+  end
+
+
+end
+
+
