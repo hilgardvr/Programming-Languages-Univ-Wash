66a1336c60524cdf5a1bf14887381570f45a2a34
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-06-16 15:51:15 +0200

    buggy cheating

--- part_c/week1/hw6assignment.rb
+++ part_c/week1/hw6assignment.rb
@@ -6,48 +6,92 @@
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
 
-#  def initialize (point_array, board)
-    #super()
-  #end
-  
   def self.next_piece (board)
-    MyPiece.new(My_Pieces.sample, board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
+
+  def self.cheat_piece (board)
+    MyPiece.new(Cheat_Piece, board)
   end
 
   # your enhancements here
-  My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]]]  # square (only needs one)
-#  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-#               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-#               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-#               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-#               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-#               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-#               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-#               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+  All_My_Pieces = [ [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+                    rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                    [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                        [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                    rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                    rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                    rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                    rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]),# Z
+                    rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]), #weird 5block
+                    [[[-2,0], [-1, 0], [0,0], [1,0], [2,0]], # long 5 horizontal
+                        [[0,-2], [0,-1], [0,0], [0,1], [0,2]]], #long 5 vertical
+                    rotations([[0, 0], [0, 1], [1, 1]]) #3 L
+                  ]
+    Cheat_Piece = [[0,0]]
 
+  # class array holding all the pieces and their rotations
+ # All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+ #              rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+ #              [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+ #              [[0, 0], [0, -1], [0, 1], [0, 2]]],
+ #              rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+ #              rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+ #              rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+ #              rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
 end
 
 class MyBoard < Board
   # your enhancements here
   def initialize (game)
-    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    super(game)
     @current_block = MyPiece.next_piece(self)
-    @score = 0
-    @game = game
-    @delay = 500
+    @cheating = false
+  end
+
+  # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    size = locations.size - 1
+    (0..(locations.size-1)).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
   end
 
   def next_piece
-    @current_block = MyPiece.next_piece(self)
+    if @cheating
+        then 
+            @current_block = MyPiece.cheat_piece(self)
+            @cheating= false
+        else 
+            @current_block = MyPiece.next_piece(self)   
+    end
     @current_pos = nil
   end
+
+  def cheat
+    if @score >= 100 and @cheating == false
+      then
+        @score= @score - 100
+        @cheating= true
+    end
+  end 
+
 end
 
 class MyTetris < Tetris
+
   # your enhancements here
   def key_bindings
     super()
-    @root.bind('u', proc {@board.rotate_counter_clockwise})
+    @root.bind('u', proc {@board.rotate_counter_clockwise; @board.rotate_counter_clockwise})
+    @root.bind('c', proc {@board.cheat})
   end
 
   def set_board
