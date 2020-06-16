989cf2d3420fa2fe655ace7e968047614621edf5
Hilgard van Rooyen <hilgardvr@gmail.com>
2020-06-16 15:54:55 +0200

    working pre submit

--- part_c/week1/hw6assignment.rb
+++ part_c/week1/hw6assignment.rb
@@ -28,17 +28,8 @@ class MyPiece < Piece
                         [[0,-2], [0,-1], [0,0], [0,1], [0,2]]], #long 5 vertical
                     rotations([[0, 0], [0, 1], [1, 1]]) #3 L
                   ]
-    Cheat_Piece = [[0,0]]
+    Cheat_Piece = [[[0,0]]]
 
-  # class array holding all the pieces and their rotations
- # All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
- #              rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
- #              [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
- #              [[0, 0], [0, -1], [0, 1], [0, 2]]],
- #              rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
- #              rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
- #              rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
- #              rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
 end
 
 class MyBoard < Board
